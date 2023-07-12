90ca96fa4085e579c6a133d79b9e7d74410372b5
Yannick Le Roux <rasyann@hotmail.com>
2023-07-12 16:56:07 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# GitHub Releases CLI
+
+The GitHub Releases CLI is a command-line tool written in Rust that allows you to check the release information for your organization's services deployed on GitHub. It fetches the repositories from your organization, retrieves the latest release for each repository, and displays the service name and last release time in two columns, sorted by release dates.
+
+## Installation
+
+To use the GitHub Releases CLI, ensure that you have Rust installed on your machine. If not, you can install it from the official Rust website (https://www.rust-lang.org/tools/install).
+
+1. Clone the repository:
+
+   - `$ git clone https://github.com/your_username/github-releases-cli.git`
+
+2. Run
+   - `$ cd github-releases-cli`
+   - `$ cargo run`
+
+The tool will fetch the repositories from your organization on GitHub, retrieve the latest release for each repository, and display the service name and last release time in two columns, sorted by release dates.
+
+## Authentication
+
+If your organization's repositories are private, you need to provide authentication. To do this, you'll need to generate a Personal Access Token (PAT) on GitHub. Follow these steps:
+
+1. Go to your GitHub account settings.
+2. Select "Developer Settings" from the sidebar.
+3. Click on "Personal access tokens".
+4. Generate a new token with the necessary permissions (here, `repo`s and `read:org`).
+5. Set the generated token as the value for the `PERSONAL_ACCESS_TOKEN` constant in the `main.rs` file.
---
+++ src/main.rs
@@ -0,0 +1,105 @@
+use reqwest::header;
+use serde::Deserialize;
+
+const ORGANIZATION: &str = "edf-re";
+const PERSONAL_ACCESS_TOKEN: &str = "your_token_here";
+
+#[derive(Deserialize)]
+struct Release {
+    published_at: Option<String>,
+}
+
+#[derive(Deserialize, Debug)]
+struct Repository {
+    name: Option<String>,
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let client = reqwest::Client::builder()
+        .user_agent("github-releases-cli")
+        .default_headers({
+            let mut headers = header::HeaderMap::new();
+            headers.insert(
+                header::AUTHORIZATION,
+                header::HeaderValue::from_str(&format!("Bearer {}", PERSONAL_ACCESS_TOKEN))?,
+            );
+            headers.insert(
+                header::ACCEPT,
+                header::HeaderValue::from_static("application/vnd.github.v3+json"),
+            );
+            headers
+        })
+        .build()?;
+
+    let mut page = 1;
+    let mut all_repositories = Vec::new();
+
+    loop {
+        let url = format!(
+            "https://api.github.com/orgs/{}/repos?page={}&per_page=100",
+            ORGANIZATION, page
+        );
+
+        let response = client.get(&url).send().await?;
+
+        if !response.status().is_success() {
+            return Err(format!("Request failed with status code: {}", response.status()).into());
+        }
+
+        let json_response = response.text().await?;
+        let repositories: Vec<Repository> = serde_json::from_str(&json_response)?;
+
+        if repositories.is_empty() {
+            break;
+        }
+
+        all_repositories.extend(repositories);
+        page += 1;
+    }
+
+    let mut releases = Vec::new();
+
+    for repo in all_repositories {
+        if let Some(name) = repo.name {
+            println!("Fetching releases for {}", name);
+            let releases_url = format!(
+                "https://api.github.com/repos/{}/{}/releases",
+                ORGANIZATION, name
+            );
+
+            let response = client.get(&releases_url).send().await?;
+
+            if !response.status().is_success() {
+                return Err(
+                    format!("Request failed with status code: {}", response.status()).into(),
+                );
+            }
+
+            let json_response = response.text().await?;
+            let repo_releases: Vec<Release> = serde_json::from_str(&json_response)?;
+
+            if let Some(last_release) = repo_releases.first() {
+                releases.push((name, last_release.published_at.clone()));
+            }
+        } else {
+            println!("Skipping repository with no name");
+        }
+    }
+
+    releases.sort_by(|a, b| b.1.cmp(&a.1));
+
+    println!("\n");
+    println!("********************************************");
+    println!("********** Latest Releases by Date *********");
+    println!("********************************************");
+    println!("\n");
+
+    for (name, published_at) in releases {
+        if let Some(release_date) = published_at {
+            println!("{:<60} \t {}", name, release_date);
+        }
+    }
+
+    Ok(())
+}
